d2f206232c6c43f7f2b357b535939feabe97350a
Hardik Modha <hardik@cronixweb.com>
2024-04-26 09:43:01 +0530

    feat: bigcommerce strapi plugin added

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = {
+  'bigcommerce': {
+    enabled: true,
+    config: {
+      accessToken: process.env.BIGCOMMERCE_ACCESS_TOKEN, // The environment variable containing your private store access token
+      shopHash: process.env.BIGCOMMERCE_SHOP_HASH, // The environment variable containing your shop hash
+    },
+  },
+}
